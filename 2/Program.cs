52e85cb461869391a9b494f1bfa251ea5074e929
Nika <krasnikova16071607@gmail.com>
2022-11-27 22:45:48 +0300

    Конец

---
+++ 2/Program.cs
@@ -0,0 +1,19 @@
+﻿int num = Convert.ToInt32(Console.ReadLine());
+
+int test = num / 100;
+
+if (test > 0)
+{
+    while (num > 999)
+    {
+        num = num / 10;
+    }
+
+    int res = num % 10;
+    Console.Write(res);
+}
+
+else
+{
+    Console.Write("Нет третьего числа");
+}
